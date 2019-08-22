0f2da920ce7aac9928e9879731b6fe5b3fb25b5e
Denis Mamedov <denismj87@gmail.com>
2019-08-21 23:24:24 -0500

    data wrangling

--- titanic/titanic_survival_rediction_v2.py
+++ titanic/titanic_survival_rediction_v2.py
@@ -5,6 +5,9 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+#pre precessing
+from sklearn import preprocessing as prep
+
 # ml models
 from sklearn.linear_model import LogisticRegression
 
@@ -28,10 +31,11 @@ test_df = pd.read_csv('data/test.csv')
 train_df.drop("Cabin", axis=1, inplace=True)
 test_df.drop("Cabin", axis=1, inplace=True)
 
-# Drop Ticket column - doesn't tell any useful information
+# Drop Ticket and Id column - doesn't tell any useful information
 train_df.drop("Ticket", axis=1, inplace=True)
 test_df.drop("Ticket", axis=1, inplace=True)
-
+train_df.drop("PassengerId", axis=1, inplace=True)
+test_df.drop("PassengerId", axis=1, inplace=True)
 
 # print(train_df.head())
 # print(test_df.head())
@@ -44,9 +48,46 @@ train_df["Age"].replace(np.nan, avg_age, inplace=True)
 test_df["Age"].replace(np.nan, avg_age, inplace=True)
 
 
-check_miss_values(train_df)
-check_miss_values(test_df)
-
 # Max frequent value in a column and replace NaN with it
 print(train_df["Embarked"].value_counts())
 train_df["Embarked"].replace(np.nan, "S", inplace=True)
+
+# 1 value is missing in Fare column and we will fill it by mean value.
+avg_fare = test_df["Fare"].mean()
+test_df["Fare"].replace(np.nan, avg_fare, inplace=True)
+
+check_miss_values(train_df)
+check_miss_values(test_df)
+print(train_df.head(5))
+
+
+#
+pclass_sex = train_df[['Survived', 'Sex', 'Pclass']]
+pclass_sex = pclass_sex.groupby(['Sex', 'Pclass'],as_index=False).mean().sort_values(by='Survived', ascending=False)
+print(pclass_sex)
+
+# Combine Siblings and Parent columns into one
+train_df["FamSize"] = train_df["SibSp"] + train_df["Parch"]
+test_df["FamSize"] = test_df["SibSp"] + test_df["Parch"]
+train_df.drop("SibSp", axis=1, inplace=True)
+test_df.drop("SibSp", axis=1, inplace=True)
+train_df.drop("Parch", axis=1, inplace=True)
+test_df.drop("Parch", axis=1, inplace=True)
+
+
+fam_group = train_df[["FamSize", "Survived"]]
+fam_group = fam_group.groupby(["FamSize"], as_index=False).mean().sort_values(by="Survived", ascending=False)
+print(fam_group)
+
+
+class_age_surv = sns.FacetGrid(train_df, col="Survived", row="Pclass")
+class_age_surv.map(plt.hist, "Age")
+plt.show()
+
+
+print(train_df.corr())
+
+print(train_df.dtypes)
+
+
+# print(train_df.dtypes)
