c41de23518a7f48b930ea63f6a6f282107bb6401
Denis Mamedov <denismj87@gmail.com>
2019-08-22 21:47:28 -0500

    data preprocess

--- titanic/titanic_survival_rediction_v2.py
+++ titanic/titanic_survival_rediction_v2.py
@@ -28,17 +28,9 @@ test_df = pd.read_csv('data/test.csv')
 
 # Check data set for missing values
 # Drop 'Cabin' col
-train_df.drop("Cabin", axis=1, inplace=True)
-test_df.drop("Cabin", axis=1, inplace=True)
-
 # Drop Ticket and Id column - doesn't tell any useful information
-train_df.drop("Ticket", axis=1, inplace=True)
-test_df.drop("Ticket", axis=1, inplace=True)
-train_df.drop("PassengerId", axis=1, inplace=True)
-test_df.drop("PassengerId", axis=1, inplace=True)
-
-# print(train_df.head())
-# print(test_df.head())
+train_df.drop(["Ticket", "PassengerId", "Cabin"], axis=1, inplace=True)
+test_df.drop(["Ticket", "PassengerId", "Cabin"], axis=1, inplace=True)
 
 # Dealing with missing values in dataset
 # Same in both data sets
@@ -49,45 +41,66 @@ test_df["Age"].replace(np.nan, avg_age, inplace=True)
 
 
 # Max frequent value in a column and replace NaN with it
-print(train_df["Embarked"].value_counts())
+# print(train_df["Embarked"].value_counts())
 train_df["Embarked"].replace(np.nan, "S", inplace=True)
 
 # 1 value is missing in Fare column and we will fill it by mean value.
 avg_fare = test_df["Fare"].mean()
 test_df["Fare"].replace(np.nan, avg_fare, inplace=True)
 
-check_miss_values(train_df)
-check_miss_values(test_df)
-print(train_df.head(5))
-
-
-#
 pclass_sex = train_df[['Survived', 'Sex', 'Pclass']]
 pclass_sex = pclass_sex.groupby(['Sex', 'Pclass'],as_index=False).mean().sort_values(by='Survived', ascending=False)
-print(pclass_sex)
+# print(pclass_sex)
 
 # Combine Siblings and Parent columns into one
 train_df["FamSize"] = train_df["SibSp"] + train_df["Parch"]
 test_df["FamSize"] = test_df["SibSp"] + test_df["Parch"]
-train_df.drop("SibSp", axis=1, inplace=True)
-test_df.drop("SibSp", axis=1, inplace=True)
-train_df.drop("Parch", axis=1, inplace=True)
-test_df.drop("Parch", axis=1, inplace=True)
+train_df.drop(["SibSp", "Parch"], axis=1, inplace=True)
+test_df.drop(["SibSp", "Parch"], axis=1, inplace=True)
 
 
 fam_group = train_df[["FamSize", "Survived"]]
 fam_group = fam_group.groupby(["FamSize"], as_index=False).mean().sort_values(by="Survived", ascending=False)
-print(fam_group)
+# print(fam_group)
 
 
 class_age_surv = sns.FacetGrid(train_df, col="Survived", row="Pclass")
 class_age_surv.map(plt.hist, "Age")
 plt.show()
 
+# Transform categorical features into ordinal
+embark_enc = prep.OrdinalEncoder()
+embark_enc.fit(train_df[["Embarked"]])
+train_df["Embarked"] = embark_enc.transform(train_df[["Embarked"]])
+test_df["Embarked"] = embark_enc.transform(test_df[["Embarked"]])
+sex_enc = prep.OrdinalEncoder()
+sex_enc.fit(train_df[["Sex"]])
+train_df["Sex"] = sex_enc.transform(train_df[["Sex"]])
+test_df["Sex"] = sex_enc.transform(test_df[["Sex"]])
 
+
+# Binning Age and Fare data
+bin_age = prep.KBinsDiscretizer(n_bins=7, encode='ordinal').fit(train_df[["Age"]])
+train_df["Age"] = bin_age.transform(train_df[["Age"]])
+test_df["Age"] = bin_age.transform(test_df[["Age"]])
+
+bin_fare = prep.KBinsDiscretizer(n_bins=5, encode='ordinal').fit(train_df[["Fare"]])
+train_df["Fare"] = bin_fare.transform(train_df[["Fare"]])
+test_df["Fare"] = bin_fare.transform(test_df[["Fare"]])
+
+print(train_df.head())
+
+# Use titles extracted from Name column and break into categories
+train_df['Title'] = train_df.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
+test_df['Title'] = test_df.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
+
+# check_miss_values(train_df)
+# check_miss_values(test_df)
+# print(train_df.head(5))
+#
 print(train_df.corr())
 
 print(train_df.dtypes)
 
 
-# print(train_df.dtypes)
+print(train_df["Title"].value_counts())
