549ee12b686e1139432e05f05261deaa919ba793
Denis Mamedov <denismj87@gmail.com>
2019-08-23 21:38:38 -0500

    lr and svm models

--- titanic/titanic_survival_rediction_v2.py
+++ titanic/titanic_survival_rediction_v2.py
@@ -9,7 +9,10 @@ import matplotlib.pyplot as plt
 from sklearn import preprocessing as prep
 
 # ml models
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+
 
 # Panda options
 pd.set_option('display.max_columns', 20)
@@ -80,7 +83,7 @@ test_df["Sex"] = sex_enc.transform(test_df[["Sex"]])
 
 
 # Binning Age and Fare data
-bin_age = prep.KBinsDiscretizer(n_bins=7, encode='ordinal').fit(train_df[["Age"]])
+bin_age = prep.KBinsDiscretizer(n_bins=5, encode='ordinal').fit(train_df[["Age"]])
 train_df["Age"] = bin_age.transform(train_df[["Age"]])
 test_df["Age"] = bin_age.transform(test_df[["Age"]])
 
@@ -88,19 +91,66 @@ bin_fare = prep.KBinsDiscretizer(n_bins=5, encode='ordinal').fit(train_df[["Fare
 train_df["Fare"] = bin_fare.transform(train_df[["Fare"]])
 test_df["Fare"] = bin_fare.transform(test_df[["Fare"]])
 
-print(train_df.head())
-
 # Use titles extracted from Name column and break into categories
 train_df['Title'] = train_df.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
 test_df['Title'] = test_df.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
 
-# check_miss_values(train_df)
-# check_miss_values(test_df)
-# print(train_df.head(5))
-#
+train_df['Title'] = train_df['Title'].replace(
+        ['Lady', 'Countess', 'Capt', 'Col', 'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+train_df['Title'] = train_df['Title'].replace('Mlle', 'Miss')
+train_df['Title'] = train_df['Title'].replace('Ms', 'Miss')
+train_df['Title'] = train_df['Title'].replace('Mme', 'Mrs')
+
+test_df['Title'] = test_df['Title'].replace(
+        ['Lady', 'Countess', 'Capt', 'Col', 'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+test_df['Title'] = test_df['Title'].replace('Mlle', 'Miss')
+test_df['Title'] = test_df['Title'].replace('Ms', 'Miss')
+test_df['Title'] = test_df['Title'].replace('Mme', 'Mrs')
+
+title_enc = prep.OrdinalEncoder()
+title_enc.fit(train_df[["Title"]])
+train_df["Title"] = title_enc.transform(train_df[["Title"]])
+test_df["Title"] = title_enc.transform(test_df[["Title"]])
+
+train_df.drop(["Name"], axis=1, inplace=True)
+test_df.drop(["Name"], axis=1, inplace=True)
+
+
 print(train_df.corr())
 
-print(train_df.dtypes)
+# Model
+# X_train = train_df.drop("Survived", axis=1)
+# Y_train = train_df["Survived"]
+# X_test = test_df.drop("PassengerId", axis=1).copy()
+
+X_train = np.asarray(train_df.drop("Survived", axis=1))
+y_train = np.asarray(train_df["Survived"])
+X_out_of_sample = np.asarray(test_df)
+
+print(X_train[0:5])
+print(y_train[0:5])
+print(X_out_of_sample[0:5])
+
+X_tr, X_ts, y_tr, y_ts = train_test_split(X_train, y_train, test_size=0.3, random_state=4)
 
 
-print(train_df["Title"].value_counts())
+X_train = prep.StandardScaler().fit(X_train).transform(X_train)
+print(X_train[0:5])
+
+
+# Trying different models
+lr = LogisticRegression(solver='liblinear')
+lr.fit(X_train, y_train)
+print(lr)
+y_test_hat_lr = lr.predict(X_out_of_sample)
+acc_lr = round(lr.score(X_train, y_train) * 100, 2)
+
+
+svm = svm.SVC(kernel='rbf')
+svm.fit(X_train, y_train)
+y_test_hat_svm = svm.predict(X_out_of_sample)
+acc_svm = round(svm.score(X_train, y_train) * 100, 2)
+
+
+print("LogReg: ", acc_lr)
+print("SVM: ", acc_svm)
