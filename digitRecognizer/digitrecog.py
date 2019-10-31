339a7c03eb1b68f6003aa80559d03781e8a8dfbd
Denis Mamedov <denismj87@gmail.com>
2019-10-30 23:43:24 -0500

    SVM with 0.97 acc

--- digitRecognizer/digitrecog.py
+++ digitRecognizer/digitrecog.py
@@ -13,6 +13,7 @@ from sklearn.decomposition import PCA
 
 # model
 from sklearn.model_selection import GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn import svm
 
 # prepossessing
@@ -49,6 +50,9 @@ def principal_component_plot(pca):
     plt.title('Pulsar Dataset Explained Variance')
     plt.show()
     print("Number of Principal components for PCA done in %0.3fs" % (time() - t0))
+    cum_var_per = pca.explained_variance_ratio_.cumsum()
+    number_of_comp = len(cum_var_per[cum_var_per <= 0.90])
+    print(" Within 90% - {}".format(number_of_comp))
 
 
 t0 = time()
@@ -70,73 +74,56 @@ t0 = time()
 X_tr, X_val, y_tr, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=1)
 print("Splitting data done in %0.3fs" % (time() - t0))
 
+# Using portion of data (for quicker computations)
+i = 15000
+print(train_df.shape)
+
+
 t0 = time()
 sc = StandardScaler().fit(X_tr)
 X_std_tr = sc.transform(X_tr)
 X_std_val = sc.transform(X_val)
 print("Normalizing data done in %0.3fs" % (time() - t0))
 
+# Picking best number of components for PCA
+pca_func = PCA().fit(X_std_tr)
+principal_component_plot(pca_func)
 
 t0 = time()
-pca = PCA(n_components=300).fit(X_std_tr)
+pca = PCA(n_components=225).fit(X_std_tr)
 train_pca = pca.transform(X_std_tr)
 val_pca = pca.transform(X_std_val)
 print("PCA done in %0.3fs" % (time() - t0))
-
 print("Shape before PCA for Train: ",X_std_tr.shape)
 print("Shape after PCA for Train: ",train_pca.shape)
 print("Shape before PCA for Test: ",X_std_val.shape)
 print("Shape after PCA for Test: ",val_pca.shape)
 
+# Picking  best C and gamma for SVM
+svm_params = {'C': [1e3, 5e3, 1e4, 5e4, 1e5],
+              'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1], }
+
 t0 = time()
-clf = svm.SVC(C=10, gamma=0.001, random_state=42)
+
+
+# clf = GridSearchCV(svm.SVC(kernel='rbf', class_weight='balanced'),
+#                    svm_params, cv=5, iid=False)
+
+clf = svm.SVC(C=1000, gamma=0.001, random_state=None)
+
 clf.fit(train_pca, y_tr)
 print("Fitting done in %0.3fs" % (time() - t0))
 
 t0 = time()
 score=clf.score(val_pca, y_val)
+# print("Best params found by grid search:")
+# print(clf.best_estimator_)
+# print("Best score found by grid search:")
+# print(clf.best_score_)
 print("Accuracy : ", score)
 print("Accuracy calculated in %0.3fs" % (time() - t0))
 
-# learn digits on training data
-# clf = svm.SVC(gamma=0.001)
-# clf.fit(X_tr, y_tr)
 print('Done training')
 
-# expected_X_val = y_tr
-# predicted_X_val = clf.predict(X_val)
-
-
-
-
-
-#
-# n_components = 150
-# print("Extracting the top %d eigenfaces from %d faces"
-#       % (n_components, X_tr.shape[0]))
-# t0 = time()
-# pca = PCA(n_components=n_components, svd_solver='randomized',
-#           whiten=True).fit(X_tr)
-# print("done in %0.3fs" % (time() - t0))
-#
-# # eigenfaces = pca.components_.reshape((n_components, h, w))
-#
-# print("Projecting the input data on the eigenfaces orthonormal basis")
-# t0 = time()
-# X_train_pca = pca.transform(X_tr)
-# X_test_pca = pca.transform(X_val)
-# print("done in %0.3fs" % (time() - t0))
-#
-#
-# print("Fitting the classifier to the training set")
-# t0 = time()
-# param_grid = {'C': [1e3, 5e3, 1e4, 5e4, 1e5],
-#               'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1], }
-# clf = GridSearchCV(svm.SVC(kernel='rbf', class_weight='balanced'),
-#                    param_grid, cv=5, iid=False)
-#
-# clf = clf.fit(X_train_pca, y_tr)
-#
-# print("done in %0.3fs" % (time() - t0))
-# print("Best estimator found by grid search:")
-# print(clf.best_estimator_)
+expected_X_val = y_val
+predicted_X_val = clf.predict(val_pca)
