062303b01ca8fe7479a5b81cb2f8d4e91b93bcf7
Denis Mamedov <denismj87@gmail.com>
2019-11-03 20:36:46 -0600

    predict test data

--- digitRecognizer/digitrecog.py
+++ digitRecognizer/digitrecog.py
@@ -78,7 +78,6 @@ print("Splitting data done in %0.3fs" % (time() - t0))
 i = 1000
 print(train_df.shape)
 
-
 t0 = time()
 sc = StandardScaler().fit(X_tr)
 X_std_tr = sc.transform(X_tr)
@@ -91,13 +90,13 @@ principal_component_plot(pca_func)
 
 t0 = time()
 pca = PCA(n_components=225).fit(X_std_tr)
-train_pca = pca.transform(X_std_tr)
+train_pca = pca.transform(X_std_tr[:i])
 val_pca = pca.transform(X_std_val)
 print("PCA done in %0.3fs" % (time() - t0))
-print("Shape before PCA for Train: ",X_std_tr.shape)
-print("Shape after PCA for Train: ",train_pca.shape)
-print("Shape before PCA for Test: ",X_std_val.shape)
-print("Shape after PCA for Test: ",val_pca.shape)
+print("Shape before PCA for Train: ", X_std_tr.shape)
+print("Shape after PCA for Train: ", train_pca.shape)
+print("Shape before PCA for Test: ", X_std_val.shape)
+print("Shape after PCA for Test: ", val_pca.shape)
 
 # Picking  best C and gamma for SVM
 svm_params = {'C': [1e3, 5e3, 1e4, 5e4, 1e5],
@@ -108,35 +107,36 @@ svm_params = {'C': [1e3, 5e3, 1e4, 5e4, 1e5],
 
 
 # number of neighbors
-knn_params = {'n_neighbors': [3, 5, 11, 19],
+knn_params = {'n_neighbors': [2, 3, 5, 7, 10, 15, 20],
               'weights': ['uniform', 'distance'],
               'metric': ['euclidean', 'manhattan']
               }
-knnclf = GridSearchCV(KNeighborsClassifier(), knn_params, verbose=1, cv=3, n_jobs=-1)
+
+# knnclf = GridSearchCV(KNeighborsClassifier(), knn_params, cv=3, n_jobs=-1)
 
 # KNN
 t0 = time()
 print("Fitting KNN...")
-# knnclf = KNeighborsClassifier()
-knnclf.fit(train_pca, y_tr)
+knnclf = KNeighborsClassifier(n_neighbors=3, weights='distance', metric='euclidean')
+knnclf.fit(train_pca, y_tr[:i])
 print("Fitting KNN done in %0.3fs" % (time() - t0))
 
 # SVM
 t0 = time()
 print("Fitting SVM...")
 clf = svm.SVC(C=1000, gamma=0.001, random_state=None)
-clf.fit(train_pca, y_tr)
+clf.fit(train_pca, y_tr[:i])
 print("Fitting SVM done in %0.3fs" % (time() - t0))
 t0 = time()
-score_svm=clf.score(val_pca, y_val)
+score_svm = clf.score(val_pca, y_val)
 score_knn = knnclf.score(val_pca, y_val)
 
-print("Best estimator found by grid search:")
-print(knnclf.best_estimator_)
-print("Best score found by grid search:")
-print(knnclf.best_score_)
-print("Best params found by grid search:")
-print(knnclf.best_params_)
+# print("Best estimator found by grid search:")
+# print(knnclf.best_estimator_)
+# print("Best score found by grid search:")
+# print(knnclf.best_score_)
+# print("Best params found by grid search:")
+# print(knnclf.best_params_)
 
 print("SVM accuracy : ", score_svm)
 print("KNN accuracy : ", score_knn)
@@ -147,3 +147,44 @@ print('Done training')
 
 expected_X_val = y_val
 predicted_X_val = clf.predict(val_pca)
+
+# Test data and submission
+
+print("Full size training data and test data")
+y_t = np.asarray(train_df["label"])
+X_t = np.asarray(train_df.drop("label", axis=1)).astype('float32')
+X_tt = np.asarray(test_df).astype('float32')
+
+t0 = time()
+sc = StandardScaler().fit(X_t)
+X_std_t = sc.transform(X_t)
+X_std_tt = sc.transform(X_tt)
+print("Normalizing data for full data (train and test)  done in %0.3fs" % (time() - t0))
+
+print("Performing PCA...")
+t0 = time()
+pca_ = PCA(n_components=225).fit(X_std_t)
+t_pca = pca_.transform(X_std_t)
+tt_pca = pca_.transform(X_std_tt)
+print("PCA done in %0.3fs" % (time() - t0))
+
+print("Fitting SVM on Full data...")
+clf_ = svm.SVC(C=1000, gamma=0.001, random_state=None)
+clf_.fit(t_pca, y_t)
+print("Fitting SVM done in %0.3fs" % (time() - t0))
+
+print("Predicting test data...")
+result = clf_.predict(tt_pca)
+print(result)
+
+
+submission_data = pd.DataFrame( {
+    'ImageId': test_df.index.values+1,
+    'Label': result
+})
+
+submission_data.index = submission_data['ImageId'].values
+
+print(submission_data.head())
+
+submission_data.to_csv('/Users/denismamedov/PycharmProjects/kaggle/digitRecognizer/data/result.csv', index = False)
