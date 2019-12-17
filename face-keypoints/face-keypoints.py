078a55df2cff6b7d266fa9daf6847582ae4c9af7
denmj <denismj87@gmail.com>
2019-12-16 22:36:25 -0600

    running model with tf-gpu

--- face-keypoints/face-keypoints.py
+++ face-keypoints/face-keypoints.py
@@ -3,17 +3,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Pre processing
+
+import tensorflow as tf
+
 from sklearn.impute import SimpleImputer
 
 from keras.layers.advanced_activations import LeakyReLU
 from keras.models import Sequential, Model
-from keras.layers import Activation, Convolution2D, MaxPooling2D, BatchNormalization, Flatten, Dense, Dropout, Conv2D,MaxPool2D, ZeroPadding2D
+from keras.layers import Activation, Convolution2D, MaxPooling2D, BatchNormalization, Flatten, Dense, Dropout, Conv2D, \
+    MaxPool2D, ZeroPadding2D
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
 
-# Splits Image array and keypoint coordinates
+# # Splits Image array and keypoint coordinates
 def split_image_data(image_data):
     imag = []
     for i in range(0, 7049):
@@ -49,6 +53,7 @@ def missing_values(df):
 
 train_df = pd.read_csv('data/training.csv')
 test_df = pd.read_csv('data/test.csv')
+
 cols = train_df.columns.values
 ids_df = pd.read_csv('data/IdLookupTable.csv')
 
@@ -60,93 +65,108 @@ ids_df = pd.read_csv('data/IdLookupTable.csv')
 
 # we could use fillna with ffill method,
 # that will fill in last valid value observed
-# train_df.fillna(method='ffill', inplace=True)
+train_df.fillna(method='ffill', inplace=True)
 
 # Or replace NaNs with mean()
-imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-train_image = split_image_data(train_df)
+# imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+# train_image = split_image_data(train_df)
+# y_train = imputer.fit_transform(train_df[cols[:-1]].to_numpy())
 
-y_train = imputer.fit_transform(train_df[cols[:-1]].to_numpy())
 
-print(train_image.shape)
-print(y_train.shape)
+imag = []
+for i in range(0, 7049):
+    img = train_df['Image'][i].split(' ')
+    img = ['0' if x == '' else x for x in img]
+    imag.append(img)
 
-X_train = train_image.reshape(-1, 96, 96, 1)
+image_list = np.array(imag, dtype='float')
+X_train = image_list.reshape(-1, 96, 96, 1)
+
 print(X_train.shape)
 
+training = train_df.drop('Image', axis=1)
+
+y_train = []
+for i in range(0, 7049):
+    y = training.iloc[i, :]
+
+    y_train.append(y)
+y_train = np.array(y_train, dtype='float')
+
+print(y_train.shape)
+
+# print(train_image.shape)
+#
+# X_train = train_image.reshape(-1, 96, 96, 1)
+# print(X_train.shape)
+#
 
 # Define model
-model = Sequential([Flatten(input_shape=(96, 96, 1)),
-                         Dense(128, activation="relu"),
-                         Dropout(0.1),
-                         Dense(64, activation="relu"),
-                         Dense(30)
-                         ])
 
-# model.add(Convolution2D(32, (3,3), padding='same', use_bias=False, input_shape=(96,96,1)))
-# model.add(LeakyReLU(alpha = 0.1))
+model = tf.keras.Sequential()
+
+model.add(tf.keras.layers.Convolution2D(32, (3, 3), padding='same', use_bias=False, input_shape=(96, 96, 1)))
+model.add(tf.keras.layers.LeakyReLU(alpha=0.1))
+model.add(tf.keras.layers.BatchNormalization())
+
+model.add(tf.keras.layers.Convolution2D(32, (3, 3), padding='same', use_bias=False))
+model.add(tf.keras.layers.LeakyReLU(alpha=0.1))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2)))
+
+model.add(tf.keras.layers.Convolution2D(64, (3, 3), padding='same', use_bias=False))
+model.add(tf.keras.layers.LeakyReLU(alpha=0.1))
+model.add(tf.keras.layers.BatchNormalization())
+
+model.add(tf.keras.layers.Convolution2D(64, (3, 3), padding='same', use_bias=False))
+model.add(tf.keras.layers.LeakyReLU(alpha=0.1))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2)))
+
+model.add(tf.keras.layers.Convolution2D(96, (3, 3), padding='same', use_bias=False))
+model.add(tf.keras.layers.LeakyReLU(alpha=0.1))
+model.add(tf.keras.layers.BatchNormalization())
+
+model.add(tf.keras.layers.Convolution2D(96, (3, 3), padding='same', use_bias=False))
+model.add(tf.keras.layers.LeakyReLU(alpha=0.1))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2)))
+
+model.add(tf.keras.layers.Convolution2D(128, (3, 3), padding='same', use_bias=False))
 # model.add(BatchNormalization())
+model.add(tf.keras.layers.LeakyReLU(alpha=0.1))
+model.add(tf.keras.layers.BatchNormalization())
 #
-# model.add(Convolution2D(32, (3,3), padding='same', use_bias=False))
-# model.add(LeakyReLU(alpha = 0.1))
-# model.add(BatchNormalization())
-# model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.Convolution2D(128, (3, 3), padding='same', use_bias=False))
+model.add(tf.keras.layers.LeakyReLU(alpha=0.1))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2)))
 #
-# model.add(Convolution2D(64, (3,3), padding='same', use_bias=False))
-# model.add(LeakyReLU(alpha = 0.1))
-# model.add(BatchNormalization())
-#
-# model.add(Convolution2D(64, (3,3), padding='same', use_bias=False))
-# model.add(LeakyReLU(alpha = 0.1))
-# model.add(BatchNormalization())
-# model.add(MaxPool2D(pool_size=(2, 2)))
-#
-# model.add(Convolution2D(96, (3,3), padding='same', use_bias=False))
-# model.add(LeakyReLU(alpha = 0.1))
-# model.add(BatchNormalization())
-#
-# model.add(Convolution2D(96, (3,3), padding='same', use_bias=False))
-# model.add(LeakyReLU(alpha = 0.1))
-# model.add(BatchNormalization())
-# model.add(MaxPool2D(pool_size=(2, 2)))
-#
-# model.add(Convolution2D(128, (3,3),padding='same', use_bias=False))
-# # model.add(BatchNormalization())
-# model.add(LeakyReLU(alpha = 0.1))
-# model.add(BatchNormalization())
-#
-# model.add(Convolution2D(128, (3,3),padding='same', use_bias=False))
-# model.add(LeakyReLU(alpha = 0.1))
-# model.add(BatchNormalization())
-# model.add(MaxPool2D(pool_size=(2, 2)))
-#
-# model.add(Convolution2D(256, (3,3),padding='same',use_bias=False))
-# model.add(LeakyReLU(alpha = 0.1))
-# model.add(BatchNormalization())
-#
-# model.add(Convolution2D(256, (3,3),padding='same',use_bias=False))
-# model.add(LeakyReLU(alpha = 0.1))
-# model.add(BatchNormalization())
-# model.add(MaxPool2D(pool_size=(2, 2)))
-#
-# model.add(Convolution2D(512, (3,3), padding='same', use_bias=False))
-# model.add(LeakyReLU(alpha = 0.1))
-# model.add(BatchNormalization())
-#
-# model.add(Convolution2D(512, (3,3), padding='same', use_bias=False))
-# model.add(LeakyReLU(alpha = 0.1))
-# model.add(BatchNormalization())
-#
-#
-# model.add(Flatten())
-# model.add(Dense(512,activation='relu'))
-# model.add(Dropout(0.1))
-# model.add(Dense(30))
+model.add(tf.keras.layers.Convolution2D(256, (3, 3), padding='same', use_bias=False))
+model.add(tf.keras.layers.LeakyReLU(alpha=0.1))
+model.add(tf.keras.layers.BatchNormalization())
+
+model.add(tf.keras.layers.Convolution2D(256, (3, 3), padding='same', use_bias=False))
+model.add(tf.keras.layers.LeakyReLU(alpha=0.1))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2)))
+
+model.add(tf.keras.layers.Convolution2D(512, (3, 3), padding='same', use_bias=False))
+model.add(tf.keras.layers.LeakyReLU(alpha=0.1))
+model.add(tf.keras.layers.BatchNormalization())
+
+model.add(tf.keras.layers.Convolution2D(512, (3, 3), padding='same', use_bias=False))
+model.add(tf.keras.layers.LeakyReLU(alpha=0.1))
+model.add(tf.keras.layers.BatchNormalization())
+
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(512, activation='relu'))
+model.add(tf.keras.layers.Dropout(0.1))
+model.add(tf.keras.layers.Dense(30))
 model.summary()
 
 model.compile(optimizer='adam',
               loss='mean_squared_error',
               metrics=['mae'])
-
 
 model.fit(X_train, y_train, epochs=50, batch_size=256, validation_split=0.2)
